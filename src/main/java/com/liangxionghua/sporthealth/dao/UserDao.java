016ad0088a8f868a1deb3029d86d3f0026caf125
Tonyleu <Tonyleu@users.noreply.github.com>
2020-09-13 23:15:29 +0800

    *）2020年9月13日课程

--- src/main/java/com/liangxionghua/sporthealth/dao/UserDao.java
+++ src/main/java/com/liangxionghua/sporthealth/dao/UserDao.java
@@ -4,7 +4,17 @@ import com.liangxionghua.sporthealth.bean.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDao {
+    public User getUpdateUser(@Param("id")Integer id);
     public User getUserByMessage(@Param("username") String username, @Param("password")String password);
+    public List<User> getAllUser(@Param("username") String username, @Param("pageStart") int pageStart, @Param("pageSize") int pageSize);
+    public int getUserCounts(@Param("username") String username);
+    public int updateState(@Param("id")Integer id, @Param("state")Boolean state);
+    public int addUser(User user);
+    public int deleteUser(@Param("id")Integer id);
+    public int updateUser(User user);
+
 }
